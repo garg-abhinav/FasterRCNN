377e1f5bf04d264dd801fe331ad837d73f467fb9
Abhinav Garg <abhinav.30garg@gmail.com>
2021-04-12 10:48:50 -0500

    rearranging files

---
+++ model/utils/bbox_tools.py
@@ -0,0 +1,111 @@
+import numpy as np
+
+def loc2bbox(src_bbox, loc):
+    if src_bbox.shape[0] == 0:
+        return np.zeros((0, 4), dtype=loc.dtype)
+
+    ## src_bbox = (y_min, x_min, y_max, x_max), it includes multiple boxes not one
+
+    ## I am not sure what this line does, it assigns the src_bbox to itself which doesn't make sense to me
+    #src_bbox = src_bbox.astype(src_bbox.dtype, copy=False)
+
+    src_height = src_bbox[:, 2] - src_bbox[:, 0]
+    src_width = src_bbox[:, 3] - src_bbox[:, 1]
+    src_ctr_y = (src_bbox[:, 0] + src_bbox[:, 2]) * 0.5
+    src_ctr_x = (src_bbox[:, 1] + src_bbox[:, 3]) * 0.5
+    
+    ## these are scales and offsets(?)
+    dy = loc[:, 0::4]
+    dx = loc[:, 1::4]
+    dh = loc[:, 2::4]
+    dw = loc[:, 3::4]
+
+    ## Simply put, numpy.newaxis is used to increase the dimension of the existing array by one more dimension, when used once.
+
+    ctr_y = dy * src_height[:, np.newaxis] + src_ctr_y[:, np.newaxis]
+    ctr_x = dx * src_width[:, np.newaxis] + src_ctr_x[:, np.newaxis]
+    h = np.exp(dh) * src_height[:, np.newaxis] #
+    w = np.exp(dw) * src_width[:, np.newaxis] #
+
+    ## destination/decoded boxes
+    dst_bbox = np.zeros(loc.shape, dtype=loc.dtype)
+    dst_bbox[:, 0::4] = ctr_y - 0.5 * h
+    dst_bbox[:, 1::4] = ctr_x - 0.5 * w
+    dst_bbox[:, 2::4] = ctr_y + 0.5 * h
+    dst_bbox[:, 3::4] = ctr_x + 0.5 * w
+
+    return dst_bbox
+    
+def bbox2loc(src_bbox, dst_bbox):
+
+### it encodes the source and destination boxes to loc
+    height = src_bbox[:, 2] - src_bbox[:, 0]
+    width = src_bbox[:, 3] - src_bbox[:, 1]
+    ctr_y = src_bbox[:, 0] + 0.5 * height
+    ctr_x = src_bbox[:, 1] + 0.5 * width
+
+    base_height = dst_bbox[:, 2] - dst_bbox[:, 0]
+    base_width = dst_bbox[:, 3] - dst_bbox[:, 1]
+    base_ctr_y = dst_bbox[:, 0] + 0.5 * base_height
+    base_ctr_x = dst_bbox[:, 1] + 0.5 * base_width
+
+    ##this should be for preventing some numerical errors
+    eps = np.finfo(height.dtype).eps
+    height = np.maximum(height, eps)
+    width = np.maximum(width, eps)
+
+    dy = (base_ctr_y - ctr_y) / height
+    dx = (base_ctr_x - ctr_x) / width
+    dh = np.log(base_height / height)
+    dw = np.log(base_width / width)
+
+    loc = np.vstack((dy, dx, dh, dw)).transpose()
+    return loc
+    
+def bbox_iou(bbox_a, bbox_b):
+    ### Args
+        #bbox_a (array): An array whose shape is (N, 4)
+            #N is the number of bounding boxes.
+            #The dtype should be numpy.float32.
+        #bbox_b (array): An array similar to bbox_a,
+            #whose shape is (K, 4)
+            #The dtype should be numpy.float32.
+    #Returns:
+    #    array:
+    #    An array whose shape is (N, K)
+    #    An element at index (n, k) contains IoUs between 
+    #    n th bounding box in bbox_a and k th bounding 
+    #    box in bbox_b.
+    if bbox_a.shape[1] != 4 or bbox_b.shape[1] != 4:
+        raise IndexError
+
+    # top left
+    tl = np.maximum(bbox_a[:, None, :2], bbox_b[:, :2]) ## I am not sure why there is none in the middle
+    # bottom right
+    br = np.minimum(bbox_a[:, None, 2:], bbox_b[:, 2:])    
+
+    area_i = np.prod(br - tl, axis=2) * (tl < br).all(axis=2)
+    area_a = np.prod(bbox_a[:, 2:] - bbox_a[:, :2], axis=1)
+    area_b = np.prod(bbox_b[:, 2:] - bbox_b[:, :2], axis=1)
+    return area_i / (area_a[:, None] + area_b - area_i)
+    
+    
+def generate_anchor_base(base_size=16, ratios=[0.5, 1, 2], anchor_scales=[8, 16, 32]):
+    py = base_size / 2.
+    px = base_size / 2.
+
+    anchor_base = np.zeros((len(ratios) * len(anchor_scales), 4),
+                           dtype=np.float32)
+    
+    for i in range(len(ratios)):
+        for j in range(len(anchor_scales)):
+            h = base_size * anchor_scales[j] * np.sqrt(ratios[i])
+            w = base_size * anchor_scales[j] * np.sqrt(1. / ratios[i])
+
+            index = i * len(anchor_scales) + j
+            anchor_base[index, 0] = py - h / 2.
+            anchor_base[index, 1] = px - w / 2.
+            anchor_base[index, 2] = py + h / 2.
+            anchor_base[index, 3] = px + w / 2.
+            
+    return anchor_base    
