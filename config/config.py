42a81c0a6aaeda1508d3e6f8c32508a4c1fd9ffb
refik-mert-cam <59484305+refik-mert-cam@users.noreply.github.com>
2021-05-14 17:15:38 -0500

    Update config.py

--- config/config.py
+++ config/config.py
@@ -1,3 +1,6 @@
+##  update this configuration file according to your network parameters
+
+
 opt = dict(
     voc_data_dir='/home/rcam2/Faster_RCNN_resnet/Faster-R-CNN-master/dataset/VOCtrainval_06-Nov-2007/VOCdevkit/VOC2007/',
     voc_data_test='/home/rcam2/Faster_RCNN_resnet/Faster-R-CNN-master/dataset/VOCtest_06-Nov-2007/VOCdevkit/VOC2007/',
@@ -6,39 +9,30 @@ opt = dict(
     num_workers=8,
     test_num_workers=8,
 
-    # sigma for l1_smooth_loss
+    # sigma values for smooth l1 loss
     rpn_sigma=3.,
     roi_sigma=1.,
 
-    # param for optimizer
-    # 0.0005 in origin paper but 0.0001 in tf-faster-rcnn
+    # parameters for optimizer
     weight_decay=0.0001,
     lr_decay=0.1,  # 1e-3 -> 1e-4
     lr=1e-3,
 
-    # visualization
-    env='faster-rcnn',  # visdom env
-    port=8097,
-    plot_every=40,  # vis every N iter
 
-    # preset
-    data='voc',
-    # pretrained_model='resnet101',  # change this to resnet101/vgg16 for pretrained model
-    pretrained_model='vgg16',
-    epoch=20,
-
-    use_adam=False,  # Use Adam optimizer
     use_chainer=False,  # try match everything as chainer
     use_drop=False,  # use dropout in RoIHead
     # debug
     debug_file='/tmp/debugf',
 
+    ## change train to false while evaluating the network
     train=True,
     test_num=10000,
     # model
     save_path='/home/rcam2/Faster_RCNN_resnet/Faster-R-CNN-master/checkpoints/checkpoint_vgg_3_scales_3_ratios_fourth',
     load_path=None,
 
+    
+    ## adjust ratios and scales accoriding to your setting.
     ratios=[0.5, 1, 2],
     anchor_scales=[8, 16, 32],
     feat_stride=16
